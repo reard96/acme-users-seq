2bfae9fe626e91661e97749d83892ddd8a72e7f8
reard96 <reardon.danielj@gmail.com>
2018-02-06 17:32:35 -0500

    more setup

--- db/index.js
+++ db/index.js
@@ -3,3 +3,7 @@ const pg = require('pg');
 const client = new pg.Client(process.env.DATABASE_URL);
 
 client.connect();
+
+const SQL_SYNC = `
+// SQLIZE HERE
+`
--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-require('./db');
+const express = require('express');
+const nunjucks = require('nunjucks');
+nunjucks.configure({ noCache: true });
+
+const app = express();
+app.use(require('method-override')('_method'));
+app.use(require('body-parser').urlencoded());
+app.set('view enginge', 'html');
+app.engine('html', nunjucks.render);
+
+const port = process.env.PORT || 3000;
+
+
+const db = require('./db');
+
+db.sync();
