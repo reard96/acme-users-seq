b4bf9ad59e00af7b8bc5e07722f21a09f7077b64
reard96 <reardon.danielj@gmail.com>
2018-02-10 06:08:04 -0500

    update to template

--- db/index.js
+++ db/index.js
@@ -5,5 +5,44 @@ const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
 
 const SQL_SYNC = `
-// SQLIZE HERE
-`
+  DROP TABLE IF EXISTS users;
+  CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255)
+  );
+`;
+
+const SQL_SEED = `
+  INSERT INTO users(name) values('superman');
+  INSERT INTO users(name) values('batman');
+  INSERT INTO users(name) values('spiderman');
+`;
+
+const seed = (cb) => {
+  client.query(SQL_SEED, cb);
+};
+
+const sync = (cb) => {
+  client.query(SQL_SYNC, cb);
+};
+
+const getUsers = (cb) => {
+  client.query('SELECT * FROM users', (err, result) => {
+    if (err) return cb(err);
+    cb(null, result.rows);
+  });
+};
+
+const getUser = (id, cb) => {
+  client.query('SELECT * FROM users WHERE id = $1', [id], (err, result) => {
+    if (err) return cb(err);
+    cb(null, result.rows.length ? result.rows[0] : null);
+  });
+}
+
+module.exports = {
+  sync,
+  getUsers,
+  seed,
+  getUser
+};
--- server.js
+++ server.js
@@ -1,16 +1,50 @@
 const express = require('express');
-const nunjucks = require('nunjucks');
-nunjucks.configure({ noCache: true });
-
 const app = express();
-app.use(require('method-override')('_method'));
-app.use(require('body-parser').urlencoded());
-app.set('view enginge', 'html');
+const nunjucks = require('nunjucks');
+nunjucks.configure({ noCache: true});
+
+app.set('view engine', 'html');
 app.engine('html', nunjucks.render);
 
 const port = process.env.PORT || 3000;
 
+app.listen(port, () => console.log(`listening on port ${port}`));
+
+app.get('/', (req, res, next) => {
+  res.render('index', { title: 'Home' });
+});
+
+app.get('/users', (req, res, next) => {
+  db.getUsers((err, users) => {
+    if (err) return next(err);
+    res.send(users);
+  });
+});
+
+app.get('/users/:id', (req, res, next) => {
+  db.getUser(req.params.id, (err, user) => {
+    if (err) return next(err);
+    res.send(user);
+  });
+});
 
 const db = require('./db');
 
-db.sync();
+db.sync((err) => {
+  if (err) return console.log(err);
+  db.getUsers ((err, users) => {
+    if (err) return console.log(err);
+    console.log(`there are ${users.length} users`);
+    db.seed((err) => {
+      if (err) return console.log(err);
+      db.getUsers((err, users) => {
+        if (err) return console.log(err);
+        console.log(`there are ${users.length} users`);
+        db.getUser(2, (err, user) => {
+          if (err) return console.log(err);
+          console.log(`user with an id of 2 is ${user.name}`);
+        });
+      });
+    });
+  });
+});
